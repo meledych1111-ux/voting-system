8d16e44ec77794da9aa358b5fc75c60f1500eb5e
meledych1111-ux <meledych1111@gmail.com>
2025-11-28 16:00:40 +0300

    Update get-emailjs-keys.js

--- api/get-emailjs-keys.js
+++ api/get-emailjs-keys.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default function handler(req, res) {
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
@@ -13,4 +13,4 @@ module.exports = async (req, res) => {
     serviceId: process.env.EMAILJS_SERVICE_ID || '',
     templateId: process.env.EMAILJS_TEMPLATE_ID || ''
   });
-};
+}
