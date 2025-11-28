fc3c218b53f8b6b2ea496c2e9951bdebcb89a131
meledych1111-ux <meledych1111@gmail.com>
2025-11-28 16:18:54 +0300

    Update get-emailjs-keys.js

--- api/get-emailjs-keys.js
+++ api/get-emailjs-keys.js
@@ -8,9 +8,17 @@ export default function handler(req, res) {
     return res.status(200).end();
   }
 
-  res.json({
+  const keys = {
     publicKey: process.env.EMAILJS_PUBLIC_KEY || '',
     serviceId: process.env.EMAILJS_SERVICE_ID || '',
     templateId: process.env.EMAILJS_TEMPLATE_ID || ''
+  };
+
+  console.log('🔑 Ключи EmailJS:', {
+    hasPublicKey: !!keys.publicKey,
+    hasServiceId: !!keys.serviceId,
+    hasTemplateId: !!keys.templateId
   });
+
+  res.json(keys);
 }
