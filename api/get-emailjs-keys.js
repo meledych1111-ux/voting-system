1d4fb1c4c69ac139c1bc1ecceba5f417e50d6d00
meledych1111-ux <meledych1111@gmail.com>
2025-11-28 15:06:14 +0300

    Create get-emailjs-keys.js

---
+++ api/get-emailjs-keys.js
@@ -0,0 +1,16 @@
+module.exports = async (req, res) => {
+  // CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  res.json({
+    publicKey: process.env.EMAILJS_PUBLIC_KEY || '',
+    serviceId: process.env.EMAILJS_SERVICE_ID || '',
+    templateId: process.env.EMAILJS_TEMPLATE_ID || ''
+  });
+};
